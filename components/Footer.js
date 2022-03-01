da5b12c9a6e5cea61cb437c8c8189abcf1e2ec91
mainick <maico.orazio@gmail.com>
2022-03-01 17:35:50 +0100

    footer component

--- components/Footer.js
+++ components/Footer.js
@@ -7,21 +7,18 @@ export default function Footer() {
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="devto" href={siteMetadata.devto} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
-          <div>{siteMetadata.author}</div>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <div>
+            <Link href="/">{siteMetadata.author}</Link>
+          </div>
         </div>
       </div>
     </footer>
