e3d23dea220956f8ba989290a55768b8c00f3328
mainick <maico.orazio@gmail.com>
2022-03-03 12:36:09 +0100

    fix

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -28,9 +28,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">
-              <Link href={companyLink} noreferrer={false}>
-                {company}
-              </Link>
+              <Link href={companyLink}>{company}</Link>
             </div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="github" href={github} />
