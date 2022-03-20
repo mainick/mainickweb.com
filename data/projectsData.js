963df3fac073e8149a2071ba0b7b1f0cf9d10d14
mainick <maico.orazio@gmail.com>
2022-03-20 21:17:30 +0100

    project page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,14 +1,14 @@
 const projectsData = [
   {
     title: 'MaiNick Web Blog',
-    description: `MaiNick Web - Il blog di Maico Orazio | Tutorial e Risorse gratuite su JavaScript, PHP e programmazione in generale.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    description: `MaiNick Web - Il mio blog | Tutorial e Risorse gratuite su JavaScript, PHP e programmazione in generale.`,
+    imgSrc: '/static/images/project-mainickweb.png',
     href: 'https://www.mainickweb.com/',
   },
   {
     title: 'React Headless Hooks',
     description: `Build and design powerful components while retaining 100% control over markup and styles.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/react-headless-hooks.png',
     href: 'https://webeetle.github.io/react-headless-hooks/',
   },
 ]
--- pages/projects.js
+++ pages/projects.js
@@ -10,10 +10,10 @@ export default function Projects() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+            Progetti
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+          <p className="sr-only text-lg leading-7 text-gray-500 dark:text-gray-400">
+            I miei progetti open source e le cose che ho costruito con i miei colleghi di lavoro
           </p>
         </div>
         <div className="container py-12">
