bb96d8095c23163bcfa1fd0c03bd9b95a2d4e17e
mainick <maico.orazio@gmail.com>
2022-03-23 17:17:51 +0100

    posts in tag page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
+  { href: '/tags/php', title: 'Php' },
+  { href: '/tags/react', title: 'React' },
+  { href: '/tags/javascript', title: 'JavaScript' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Progetti' },
   { href: '/about', title: 'About' },
