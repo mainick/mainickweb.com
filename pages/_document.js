151c6a096e6bf8bc37d45d6b3a079c7398fb4ef2
mainick <maico.orazio@gmail.com>
2022-03-28 18:45:41 +0200

    GitHub action deploy feed xml

--- pages/_document.js
+++ pages/_document.js
@@ -29,10 +29,14 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href={prefixImage + siteMetadata.favicon.ico} />
           <meta name="apple-mobile-web-app-title" content={siteMetadata.title} />
           <meta name="application-name" content={siteMetadata.title} />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link
+            rel="mask-icon"
+            href={prefixImage + '/static/favicons/safari-pinned-tab.svg'}
+            color="#5bbad5"
+          />
           <meta name="msapplication-TileColor" content="#337ab7" />
           <meta name="theme-color" content="#FFFFFF" />
-          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <link rel="alternate" type="application/rss+xml" href={prefixImage + '/feed.xml'} />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
