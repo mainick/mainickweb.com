4de3e667a08ff2e91c17fe8c6a8a8d392d652ade
mainick <maico.orazio@gmail.com>
2022-03-31 16:30:07 +0200

    env deploy

--- next.config.js
+++ next.config.js
@@ -86,4 +86,7 @@ module.exports = withBundleAnalyzer({
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    domains: ['mainick.github.io'],
+  },
 })
