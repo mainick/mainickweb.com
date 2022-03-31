1879e5bd8a6854c567ed32898550e8741a0ec4dc
mainick <maico.orazio@gmail.com>
2022-03-31 16:37:47 +0200

    env deploy

--- components/Image.js
+++ components/Image.js
@@ -1,15 +1,6 @@
-import NextImage from 'next/image'
-
 // eslint-disable-next-line jsx-a11y/alt-text
 const prefixImage = process.env.NEXT_PUBLIC_BASE_PATH || ''
-const deployPlatform = process.env.NEXT_PUBLIC_DEPLOY_PLATFORM || 'vercel'
 
-const Image = ({ src, ...rest }) => {
-  return deployPlatform === 'vercel' ? (
-    <NextImage {...rest} />
-  ) : (
-    <img src={prefixImage + src} {...rest} />
-  )
-}
+const Image = ({ src, ...rest }) => <img src={prefixImage + src} {...rest} />
 
 export default Image
