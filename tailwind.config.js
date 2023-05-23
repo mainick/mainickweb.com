54687ea339f40dac8690aaf07df3d6798cc95d75
mainick <maico.orazio@gmail.com>
2023-05-23 10:28:24 +0200

    new color background

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
       colors: {
         primary: colors.sky,
         gray: colors.gray,
+        'green-bottle': '#172b36',
       },
       typography: (theme) => ({
         DEFAULT: {
