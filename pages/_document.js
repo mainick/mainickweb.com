19653dddb13d66588a3b4da0bcaffdde599214bf
mainick <maico.orazio@gmail.com>
2023-05-23 10:34:47 +0200

    new color background

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#FFFFFF" />
           <link rel="alternate" type="application/rss+xml" href={prefixImage + '/feed.xml'} />
         </Head>
-        <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white green-bottle">
+        <body className="bg-white text-black antialiased green-bottle dark:text-white">
           <Main />
           <NextScript />
         </body>
